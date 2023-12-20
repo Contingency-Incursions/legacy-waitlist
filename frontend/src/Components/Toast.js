c164cd63ced7f606418a9bcc5593047eb0c89c68
Alex Dent <aldent95@hotmail.com>
2023-12-20 16:14:07 +1300

    Fixing toast z index

--- frontend/src/Components/Toast.js
+++ frontend/src/Components/Toast.js
@@ -13,7 +13,7 @@ const ToastDOM = styled.div`
   transition-timing-function: ease-in-out;
   transition-duration: 0.3s;
   box-sizing: content-box;
-  z-index: 101;
+  z-index: 201;
 `;
 ToastDOM.Container = styled.div`
   > ${ToastDOM} {
